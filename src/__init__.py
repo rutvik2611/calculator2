da684794dba7c5b4f972700f14884f6f1c5f09b0
rutvik2611 <rutvik2611@gmail.com>
2019-12-14 15:40:06 -0500

    Fix:run python

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import bcrypt
 
 # For initializing our db
 db = SQLAlchemy()
 
-bcrypt = Bcrypt()
+bcrypt = bcrypt()
