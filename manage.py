0dd44383ff802a255a6cea64bc13f46fef457f22
rutvik2611 <rutvik2611@gmail.com>
2019-12-14 21:31:28 -0500

    Add:Self Genrated  migrations folder,inspection profile and misc.xml

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
 from flask_script import Manager
-from flask_migrate import Migrate, Migrate
+from flask_migrate import Migrate, MigrateCommand
 
 from src.app import create_app, db
 
@@ -11,7 +11,7 @@ migrate = Migrate(app=app, db=db)
 
 manager = Manager(app=app)
 
-manager.add_command('db', Migrate)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
   manager.run()
