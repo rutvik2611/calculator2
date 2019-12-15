deff15440ff009e94ec4eeb4c7997faf65c19710
rutvik2611 <rutvik2611@gmail.com>
2019-12-14 20:58:14 -0500

    Update:app python

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 from .config import app_config
 
+from .models import db, bcrypt
+
 
 def create_app(env_name):
     """
@@ -14,6 +16,10 @@ def create_app(env_name):
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
+
+    bcrypt.init_app(app)
+
+    db.init_app(app)
 
     @app.route('/', methods=['GET'])
     def index():
