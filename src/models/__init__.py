9c17c90d58487afc05df1a95e4c083b36e0e9611
rutvik2611 <rutvik2611@gmail.com>
2019-12-14 20:58:43 -0500

    Update:init form model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,13 @@
 #src/models/__init__.py
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # initialize our db
 db = SQLAlchemy()
+
+
+#######
+# existing code remains #
+#######
+bcrypt = Bcrypt()
