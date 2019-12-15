f9a784fbb77a849b587fb57f5308bff6aa14e29c
rutvik2611 <rutvik2611@gmail.com>
2019-12-15 00:18:13 -0500

    Add:Blogpost view(get one method)

--- src/views/BlogpostView.py
+++ src/views/BlogpostView.py
@@ -23,6 +23,17 @@ def create():
   data = blogpost_schema.dump(post).data
   return custom_response(data, 201)
 
+@blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
+def get_one(blogpost_id):
+  """
+  Get A Blogpost
+  """
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  return custom_response(data, 200)
+
 @blogpost_api.route('/', methods=['GET'])
 def get_all():
   """
